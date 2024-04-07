d80689ec49a9acd6dae550135605243f1e090b2c
fogaca.lf <fogaca.lf@gmail.com>
2024-04-07 19:47:56 -0300

    main page

---
+++ src/pages/main/main.js
@@ -0,0 +1,17 @@
+const casamentoExemplo = {
+	noiva: 'Maria',
+	noivo: 'João',
+	data: '01/01/2021',
+	local: 'Igreja São José',
+	horario: '10:00',
+};
+
+const casamentoExemplo2 = {
+	noiva: 'Ana',
+	noivo: 'Pedro',
+	data: '02/02/2022',
+	local: 'Igreja São Pedro',
+	horario: '11:00',
+};
+
+let listaCasamentos = [casamentoExemplo, casamentoExemplo2];
