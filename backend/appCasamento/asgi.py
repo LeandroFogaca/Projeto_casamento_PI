7b4a77c7952af4b8148f38e9c892b37f7c8b62a6
fogaca.lf <fogaca.lf@gmail.com>
2024-09-06 18:29:31 -0300

    django-admin startproject appCasamento .

--- backend/appCasamento/asgi.py
+++ backend/appCasamento/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for casamento project.
+ASGI config for appCasamento project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'casamento.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'appCasamento.settings')
 
 application = get_asgi_application()
--- backend/appCasamento/wsgi.py
+++ backend/appCasamento/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for casamento project.
+WSGI config for appCasamento project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'casamento.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'appCasamento.settings')
 
 application = get_wsgi_application()
