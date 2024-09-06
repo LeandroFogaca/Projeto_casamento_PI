93e6ad34d66286ecc6ae18b6b0ed4b38abb19d48
fogaca.lf <fogaca.lf@gmail.com>
2024-09-06 19:29:47 -0300

    fix estrutura pastas

---
+++ backend/appCasamento/server_api/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Evento(models.Model):
+    noiva = models.CharField(max_length=100)
+    noivo = models.CharField(max_length=100)
+    email = models.EmailField()
+    endereco = models.CharField(max_length=255)
+    data = models.DateField()
+    lista = models.JSONField()
+
+    def __str__(self):
+        return f"{self.noiva} e {self.noivo}"
